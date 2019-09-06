3bdbd036290b5533e90c23082b23127ed948b34a
Edgardo Santana <javeds@MacBook-Pro.local>
2019-09-06 00:45:10 -0500

    ES-2019-09-05_Cors.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const client_config = require('dotenv').config().parsed;
+var cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -8,6 +9,22 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(bodyParser.json())
+
+let allowedOrigins = ['http://localhost:3000',
+    'http://localhost:4200'
+];
+
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.get('/', (req, res) => {
     res.json({
